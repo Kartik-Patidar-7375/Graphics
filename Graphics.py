4af4214b798ae934d805169fc2deccf16681439b
Kartik Patidar <168258403+Kartik-Patidar-7375@users.noreply.github.com>
2024-04-29 10:10:09 +0530

    Add files via upload

---
+++ Graphics.py
@@ -0,0 +1,65 @@
+# Code with Kartik
+
+
+
+# All Rights Reserved
+
+
+
+# Code Starts Here
+
+from turtle import *
+
+import colorsys
+
+bgcolor('black')
+
+tracer(500)
+
+
+
+def draw():
+
+    h = 0
+
+    for i in range(100):
+
+        c = colorsys.hsv_to_rgb(h,1,1)
+
+        h += 0.5
+
+        up()
+
+        goto(0,0)
+
+        down()
+
+        color('black')
+
+        fillcolor (c)
+
+        begin_fill()
+
+        rt (98)
+
+        circle(i, 12)
+
+        fd (290)
+
+        fd(i)
+
+        lt (29)
+
+        for j in range(129):
+
+            fd(i)
+
+            circle(j, 299, steps=2)
+
+        end_fill()
+
+
+
+draw()
+
+done()
