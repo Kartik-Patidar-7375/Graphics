8ece4b18224c69c3ab989600fb51e9bda7229277
Kartik Patidar <168258403+Kartik-Patidar-7375@users.noreply.github.com>
2024-05-07 08:46:55 +0530

    Create Graphics_5.py

---
+++ Graphics_5.py
@@ -0,0 +1,37 @@
+# Code with Kartik
+
+
+
+# All Rights Reserved
+
+
+
+# Code Starts Here
+
+import turtle
+
+screen = turtle.Screen()
+
+screen.bgcolor("black")  # Set background color to black
+
+t = turtle.Turtle()
+
+t.speed(0) 
+
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+for i in range(360):
+
+    t.color(colors[i % 7])
+
+    t.width(i/100 + 1)      
+
+    t.forward(i)
+
+    t.left(59)
+
+t.hideturtle()
+
+turtle.done()
+
+# Code Ends Here
